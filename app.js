1bc1f63d725388ee4f0ce167ab864f03761115b0
M4D P0IS0N <viniciusmininguelo28@hotmail.com>
2022-05-07 13:37:47 -0300

    App.get update.

--- app.js
+++ app.js
@@ -137,11 +137,11 @@ app.get("/about", function(req, res){
 
 
 
-let port = process.env.PORT;
+const port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log("Server has started successfully");
 });
